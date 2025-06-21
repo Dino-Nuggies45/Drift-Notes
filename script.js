e6453189d70670a8ab0d9ff9c0c0dfa450a42225
Dino-Nuggies45 <danamitchell1265@gmail.com>
2025-06-20 17:53:42 -0700

    message function and categories

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const noteInput = document.getElementById("noteInput");
 const sendBtn = document.getElementById("sendBtn");
 const fishBtn = document.getElementById("fishBtn");
 const fishedNote = document.getElementById("fishedNote");
+const noteType = document.getElementById("noteType");
+let fishedIDs = new Set();
 
 if (!localStorage.getItem("driftNotes")){
     localStorage.setItem("driftNotes", JSON.stringify([]));
@@ -11,32 +13,48 @@ function getNotes(){
     return JSON.parse(localStorage.getItem("driftNotes")) || [];
 }
 
-function saveNote(note) {
+function saveNote(noteText, type = "misc") {
     const currentNotes = getNotes();
-    currentNotes.push(note);
+    const newNote = {
+        id: Date.now(),
+        text: noteText,
+        type: type,
+        timestamp: new Date().toISOString()
+    }
+    currentNotes.push(newNote);
     localStorage.setItem("driftNotes", JSON.stringify(currentNotes));
 }
 
 sendBtn.addEventListener("click", () => {
     const note = noteInput.value.trim();
+    const type = noteType.value;
+
     if (note.length === 0){
         alert("Please write something before sending!")
         return;
     }
 
-    saveNote(note);
+    saveNote(note, type);
     noteInput.value = "";
     alert("Your note has been set adrift the ocean")
 });
 
+document.getElementById("resetFish").addEventListener("click", () => {
+    fishedIDs.clear();
+    fishedNote.textContent = "The sea stirs again...";
+})
+
 fishBtn.addEventListener("click", () => {
     const allNotes = getNotes();
+
+    const availableNotes = allNotes.filter(n => !fishedIDs.has(n.id));
     
-    if (allNotes.length === 0) {
-        fishedNote.textContent = "No notes drifting yet...be the first!";
+    if (availableNotes.length === 0) {
+        fishedNote.textContent = "You've fished up everything! The sea feels empty...";
         return;
     }
 
-    const randomNote = allNotes[Math.floor(Math.random() * allNotes.length)];
-    fishedNote.textContent = `"${randomNote}"`;
+    const randomNote = availableNotes[Math.floor(Math.random() * availableNotes.length)];
+    fishedIDs.add(randomNote.id);
+    fishedNote.textContent = `"${randomNote.text || '[No message found]'}" - (${randomNote.type || 'misc'})`;
 });
